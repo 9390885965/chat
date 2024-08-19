573ea1c292ce13d839609f41bd2dcaac706a15a2
unknown <gopijadda41@gmail.com>
2024-08-19 12:47:47 +0530

    update the alert message

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ function App() {
         setIsRegistering(false);
         setError('');
       }
-      alert("login successfully...");
+      alert("login successfully ....");
     } catch (error) {
       setError('Registration failed');
      
